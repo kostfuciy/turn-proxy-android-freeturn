201adf4c4463dc4bfe0d30629a66c36a978333ef
samosvalishe <samosvalishe@users.noreply.github.com>
2026-06-08 00:09:59 +0300

    chore: cleanup

--- app/src/main/java/com/freeturn/app/ui/theme/Theme.kt
+++ app/src/main/java/com/freeturn/app/ui/theme/Theme.kt
@@ -109,7 +109,6 @@ private val darkScheme = darkColorScheme(
 @Composable
 fun FreeTurnTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
